52ede76a791e1048e5882a203b4c154aa6cbca9c
Navid Pazoki <navid.pazoki@gmx.de>
2024-02-04 21:39:29 +0100

    Included function in 'dialog-userselection' to add the company ID to the user assignment

--- src/app/shared/dialogs/dialog-userselection/dialog-userselection.component.ts
+++ src/app/shared/dialogs/dialog-userselection/dialog-userselection.component.ts
@@ -7,6 +7,8 @@ import { CompanyData } from '../../../shared/interfaces/company-data';
 import { UsersSelectionListComponent } from '../../components/users-selection-list/users-selection-list.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { UserData } from '../../interfaces/user-data';
+import { first } from 'rxjs';
 
 
 @Component({
@@ -57,12 +59,33 @@ export class DialogUserselectionComponent {
   }
 
   async save() {
-    this.oldList = this.userList.selectedUserIds;
     this.data.companyData.assigned = this.userList.selectedUserIds;
     this.loading = true;
-    await this.FBServices.updateCopmanyData(this.data.companyData);
+    this.setAssignedAtUsers();
+    await this.FBServices.updateCopmanyData(this.data.companyData);    
+    this.oldList = this.userList.selectedUserIds;
     this.loading = false;
     this.closeDialog();
+  }
+
+
+  setAssignedAtUsers(){
+    let toAdd:string[] = this.compareArrays(this.userList.selectedUserIds,this.oldList);
+    let users: UserData[] =  this.userList.getUsers();
+    for (let i = 0; i < users.length; i++) {
+      let id = users[i].id;
+      if (!id) return
+      if (toAdd.indexOf(id) !== -1)  users[i].assigned.push(id);
+    }
+      
+    let toDellet = this.compareArrays(this.oldList, this.userList.selectedUserIds);
+
+  }
+
+
+  compareArrays(NewArray:string[], OldArray:string[]) {
+    let uniqueInNewArray = NewArray.filter(item => !OldArray.includes(item));  
+    return uniqueInNewArray
   }
 
   closeDialog(){
